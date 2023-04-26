642ae61449a10a0068674d6a944f6f3e917d9349
Connor Meehan <18220073+cmeehan1991@users.noreply.github.com>
2023-04-26 14:10:46 +0000

    Updating portfolio and resume

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 	
-	$page = 'home';
+	$page = 'template';
 
 	$pageTitle = 'Hi! I&#39;m Madeline. I am a graphic designer, web desinger, and an illustrator.';
 
@@ -8,16 +8,15 @@
 
 	$pageKeyword = 'hire a designer, i need a website, graphic designer 28205, charlotte graphic designer, charlotte designer, charlotte nc graphic design, i need a logo, graphic designer charlotte nc';
 
-	include('bootstrap/inc/headcode.html'); 
+	include('inc/headcode.html'); 
 
 ?>
-				<h1>I am building a website!</h1>
-					<p>I'm currently building my portfolio website for a class project.  I'm buildinng it in phases, and I'll be updating this page as I make progress.  CHeck back to keep up with my progress if you're interested.</p>
-				<h2> Progress Updates</h2>
-					<ol> 
-						<b>2.09</b><li>I have set up my workspace so that I have the basics in place to make a website.  You can <a href="workspace-setup.php" title="see the steps I took to set up my workspace so that I can start making a website"> see what i've accomplished on my Workspace Setup Page. </a>
-						</li>
-					</ol>
+				<h1>Looking to hire a designer? I may be able to help.</h1>
+				<p>&hellip;</p>
+				<p>&hellip;</p>
+				<p>&hellip;</p>
+					<p>I am a designer from Charlotte, NC.  I grew up loving illustration, and have turned that passion into a career. I have expierence in a number of design fields, including graphc design and web design.  <a href="portfolio.php">Let me show you some of my work so that you can learn more about how I solve problems with a balance of creativity and pragmatism.</p>
+				
 
 			
 			<?php 
---
+++ resume.php
@@ -0,0 +1,130 @@
+<?php 
+	
+	$page = 'resume';
+
+	$pageTitle = 'View my design resume!';
+
+	$pageDesc = 'I have a lot of expierence in several different fields, not because I lack focus.  I love learning new things, and I love helping others do the same. Please take a look over my Resume';
+
+	$pageKeyword = 'Resume, hire a designer, graphic design resume, charlotte graphic designer, logo designer';
+
+	include('inc/headcode.html'); 
+	
+	
+?>
+				<!-- This is the content that will be unique to each page  -->
+				
+<div class="container">
+	
+	<div class="row">
+		<div class="col-12 mx-auto">
+			<h1 class="resume-title">Madeline Lippert</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col mx-auto">
+			<h2>GRAPHIC DESIGNER</h2>
+		</div>
+		<div class="col-8 mx-auto">
+			<hr/>
+		</div>
+	</div>
+	<div class="row resume-body">
+		<div class="col-6">
+			<div class="row">
+				<div class="col">
+					<img src="/img/ML-logo.png" alt="Madeline Lippert's Logo" class="resume-logo"/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<h3>PERSONAL PROFILE</h3>
+					<p>Hello! I'm Madeline, a professional and knowledgeable graphic designer with over 2 decades of illustration experience. Highly efficient and accurate with strong problem- solving, communication and organizational skills. Dedicated to serve excellence, client satisfaction, and team collaboration.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<h3>CONTACT</h3>
+					<p>
+						<a href="mailto:contact@madelinelippert.com">contact@madelinelippert.com</a><br/>
+						<a href="tel:7046449414">704-644-9414</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="row">
+				<div class="col">
+					<div class="row">
+						<div class="col">
+							<h3>JOB EXPERIENCE</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-2"><strong>2023</strong></div>
+						<div class="col-10">
+							<h4>CBM Web Development Junior Graphic Designer</h4>
+							<p>Graphic Designer</p>
+							<ul>
+								<li>Tasked to make graphics for online and offline layouts.</li>
+								<li>Edited photos for clients and social media posts.</li>
+								<li>Research and copywriting.</li>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-2"><strong>2013</strong></div>
+						<div class="col-10">
+							<h4>lllilll Stitches</h4>
+							<p>Founder and Owner</p>
+							<ul>
+								<li>Worked closely with clients team to produce beautifully designed, chain stitched designs.</li>
+								<li>Created and organized shopping event concepts for major clients, such as Madewell and Anthropology.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<div class="row">
+						<div class="col">
+							<h3>EDUCATION</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-2"><strong>2023</strong></div>
+						<div class="col-10">
+							<h4>Central Piedmont Community College</h4>
+							<p>Advertising and Graphic Design</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<h3>SKILLS</h3>
+					<ul>
+						<li>Logo Branding</li>
+						<li>Graphic Design</li>
+						<li>Illustration &amp; Freehand</li>
+						<li>Typography</li>
+						<li>Photography</li>
+						<li>Copywriting</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+				<a href="downloads/Madeline_Lippert_Resume.pdf" title="Resume Download">Download a Copy of My Resume</a>
+
+			
+			<?php 
+
+	include('inc/footcode.html'); 
+
+?>
+				
+
